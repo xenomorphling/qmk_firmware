46e027aca15ff49813d6139b5908338538ee0bd2
Yury Matsyuk <ymatsyuk@luxoft.com>
2020-11-27 13:26:07 +0300

    mirror lily58

--- keyboards/lily58/keymaps/ninjacat/keymap.c
+++ keyboards/lily58/keymaps/ninjacat/keymap.c
@@ -62,20 +62,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 
 
-//  [_QWERTY] = LAYOUT( \
-//   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC, \
-//   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
-//   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-//   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
-//                              KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI \
-// ),
-
- [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_5,    KC_4,    KC_3,    KC_2,    KC_1,    KC_ESC, \
+/* [_QWERTY] = LAYOUT( \
+  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RBRC, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_GRV,  KC_RBRC,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
                              KC_LALT, KC_LGUI,LOWER, KC_SPC,   KC_ENT,   RAISE,   KC_BSPC, KC_RGUI \
+),
+*/
+
+ [_QWERTY] = LAYOUT( \
+  KC_RBRC,  KC_0,     KC_9,    KC_8,    KC_7,    KC_6,                     KC_5,    KC_4,    KC_3,    KC_2,    KC_1,    KC_ESC, \
+  KC_LBRC,  KC_P,     KC_O,    KC_I,    KC_U,    KC_Y,                     KC_T,    KC_R,    KC_E,    KC_W,    KC_Q,    KC_TAB, \
+  KC_QUOT,  KC_SCLN,  KC_L,    KC_K,    KC_J,    KC_H,                     KC_G,    KC_F,    KC_D,    KC_S,    KC_A,    KC_LCTRL, \
+  KC_RSFT,  KC_SLSH,  KC_DOT,  KC_COMM, KC_M,    KC_N,  KC_RBRC,  KC_GRV,  KC_B,    KC_V,    KC_C,    KC_X,    KC_Z,    KC_LSFT, \
+                               KC_RGUI, KC_BSPC, RAISE, KC_ENT,   KC_SPC,  LOWER,   KC_LGUI, KC_LALT \
 ),
 
 
